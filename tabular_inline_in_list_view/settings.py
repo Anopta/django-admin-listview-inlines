79bbe1a0840c7d8ca8e3ce40e70e3c99d10ef311
Anopta <66787374+Anopta@users.noreply.github.com>
2022-07-19 15:57:02 +0900

    created admin

--- tabular_inline_in_list_view/settings.py
+++ tabular_inline_in_list_view/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'app1',
 ]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
